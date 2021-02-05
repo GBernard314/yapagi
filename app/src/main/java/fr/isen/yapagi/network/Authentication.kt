04fe396aa1058ec342aab6d7ea326489e4a8513e
yadev83 <50366613+yadev83@users.noreply.github.com>
2021-02-05 13:22:40 +0100

    Adds Login/Logout/Register methods to Authentication class

--- app/src/main/java/fr/isen/yapagi/network/Authentication.kt
+++ app/src/main/java/fr/isen/yapagi/network/Authentication.kt
@@ -13,20 +13,26 @@ import com.google.firebase.ktx.Firebase
 class Authentication(val parentActivity: Activity) {
     companion object{
         private const val TAG = "AUTHENTICATION"
-    }
-    private var auth: FirebaseAuth = Firebase.auth
 
-    private var user: FirebaseUser? = null
-        get(){
-            return user
+        private var auth: FirebaseAuth = Firebase.auth
+        //user is null if the User is signed out. The user is returned otherwise
+        private var user: FirebaseUser? = null
+            get(){
+                updateUser()
+                return user
+            }
+
+        private fun updateUser(){
+            user = auth.currentUser
         }
+    }
 
     fun createUser(email: String, password: String){
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(parentActivity) { task ->
                 if(task.isSuccessful){
                     Log.d(TAG, "createUser::CREATED_USER_WITH_SUCCESS")
-                    user = auth.currentUser
+                    updateUser()
                     //Snackbar.make(view, R.string.user_create_success, Snackbar.LENGTH_SHORT).show()
                     //DO WHAT IS NEEDED WITH THE NEWLY CREATED USER
                 }else{
@@ -37,15 +43,24 @@ class Authentication(val parentActivity: Activity) {
             }
     }
 
-    fun isSignedIn(): Boolean{
-        var result = false
+    fun loginUser(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(parentActivity) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "loginUser::SIGNED_IN_USER_WITH_SUCCESS")
+                    //Snackbar.make(view, R.string.user_log_success, Snackbar.LENGTH_SHORT).show()
+                    updateUser()
+                    //WHAT ELSE ?
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.d(TAG, "loginUser::SIGNED_IN_USER_FAILED", task.exception)
+                    //Snackbar.make(view, R.string.user_log_failed, Snackbar.LENGTH_SHORT).show()
+                }
+            }
+    }
 
-        user = auth.currentUser
-        if(user != null){
-            Log.d(TAG, "User is already signed in")
-            result = true
-        }
-
-        return result
+    fun logoutUser(){
+        Firebase.auth.signOut()
+        updateUser()
     }
 }
