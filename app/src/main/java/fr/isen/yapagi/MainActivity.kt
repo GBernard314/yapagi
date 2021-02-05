22934ed895b61ebb4a81321ef077216a87f324fc
Perifanos <smaniotto.gabriel@gmail.com>
2021-02-05 11:50:15 +0100

    Landing page + sharedPreferences clear, save, load

--- app/src/main/java/fr/isen/yapagi/MainActivity.kt
+++ app/src/main/java/fr/isen/yapagi/MainActivity.kt
@@ -1,7 +1,9 @@
 package fr.isen.yapagi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.isen.yapagi.connection.LandingActivity
 import fr.isen.yapagi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, LandingActivity::class.java)
+        startActivity(intent)
     }
 }
