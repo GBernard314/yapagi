8489f12115c4eb24856e9696002989a11b750d9d
DESOOMER Pierre <p.desoomer@gmail.com>
2021-02-05 15:22:02 +0100

    Username is not received from user preferences

--- app/src/main/java/fr/isen/yapagi/fragments/LoginFragment.kt
+++ app/src/main/java/fr/isen/yapagi/fragments/LoginFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.afollestad.vvalidator.form
 import fr.isen.yapagi.R
 import fr.isen.yapagi.databinding.FragmentLoginBinding
