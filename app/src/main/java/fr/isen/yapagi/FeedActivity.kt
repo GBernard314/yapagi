ec685afe47e41a8f993c4fadaca2074c077e8041
BERNARD Guillaume <34131124+GBernard314@users.noreply.github.com>
2021-02-05 11:12:35 +0100

    feed activity working need improvements

--- app/src/main/java/fr/isen/yapagi/FeedActivity.kt
+++ app/src/main/java/fr/isen/yapagi/FeedActivity.kt
@@ -3,7 +3,11 @@ package fr.isen.yapagi
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
+import fr.isen.yapagi.data.Post
 import fr.isen.yapagi.databinding.ActivityFeedBinding
+import org.json.JSONException
+import org.json.JSONObject
 
 lateinit var binding_feed: ActivityFeedBinding
 class FeedActivity : AppCompatActivity() {
@@ -17,5 +21,49 @@ class FeedActivity : AppCompatActivity() {
 
         binding_feed.postList.layoutManager = LinearLayoutManager(this)
 
+        makeRequest()
     }
+    private fun makeRequest() {
+
+        var list: List<Post> = emptyList()
+        list += Post("1st post")
+        println("list = $list")
+
+        binding_feed.postList.adapter = FeedRecyclerViewAdapter(list, this)
+
+        /*
+        val gson: Post =
+        Gson().fromJson(response.toString(), DishDetailData::class.java)
+        gson.data.firstOrNull { it.category == "Entrées" }?.dish?.let {
+            binding.starterList.adapter = StarterRecycleViewAdapter(it, this);
+        }
+        val queue = Volley.newRequestQueue((this))
+        var url = "http://test.api.catering.bluecodegames.com/menu"
+        var postData = JSONObject()
+        var jsonRet = ""
+        try {
+            postData.put("id_shop", "1")
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.POST,
+            url,
+            postData,
+            {  response ->
+                val gson: DishDetailData =
+                    Gson().fromJson(response.toString(), DishDetailData::class.java)
+                gson.data.firstOrNull { it.category == "Entrées" }?.dish?.let {
+                    binding.starterList.adapter = StarterRecycleViewAdapter(it, this);
+                }
+            },
+            { error ->
+                error.printStackTrace()
+            })
+        queue.add(jsonObjectRequest)
+
+         */
+    }
+
+
 }
