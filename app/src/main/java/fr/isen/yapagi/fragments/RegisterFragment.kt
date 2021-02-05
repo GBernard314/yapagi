755c6296f2b8fad776dc9f9e34f45af0a9705be6
DESOOMER Pierre <p.desoomer@gmail.com>
2021-02-05 11:01:34 +0100

    Added form verification to the register fragment and changed register fragment layout

--- app/src/main/java/fr/isen/yapagi/fragments/RegisterFragment.kt
+++ app/src/main/java/fr/isen/yapagi/fragments/RegisterFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.afollestad.vvalidator.form
 import fr.isen.yapagi.R
+import fr.isen.yapagi.databinding.FragmentRegisterBinding
+
+private lateinit var binding: FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
 
@@ -13,8 +18,41 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        form {
+            input(binding.etName){
+                isNotEmpty()
+                length().atLeast(3)
+            }
+            input(binding.etEmail){
+                isNotEmpty()
+                length().atLeast(6)
+                matches("^[a-zA-Z0-9.!#\$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\$")
+            }
+            input(binding.etPassword){
+                isNotEmpty()
+                length().atLeast(8)
+            }
+            input(binding.etUsername){
+                isNotEmpty()
+                length().atLeast(5)
+            }
+
+            submitWith(binding.btnRegister) { result ->
+                //sendUserToAPI()
+                displayToast("Registered")
+
+            }
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    fun displayToast(str: String) {
+        Toast.makeText(activity?.applicationContext, str, Toast.LENGTH_SHORT).show();
     }
 
 }
