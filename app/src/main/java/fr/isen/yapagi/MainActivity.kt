067d4f51b1eb90751df0fc8761cb7618f35260af
yadev83 <50366613+yadev83@users.noreply.github.com>
2021-02-05 10:14:33 +0100

    Hotfix - Adds Viewbinding in gradle && Removes .attia. from package name that were left

--- app/src/main/java/fr/isen/yapagi/MainActivity.kt
+++ app/src/main/java/fr/isen/yapagi/MainActivity.kt
@@ -2,8 +2,11 @@ package fr.isen.yapagi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.isen.yapagi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
