271693a7863297475efc606fd295f61e926dabd1
yadev83 <50366613+yadev83@users.noreply.github.com>
2021-02-05 11:11:06 +0100

    Adds firebase configuration

---
+++ app/src/main/java/fr/isen/yapagi/network/Database.kt
@@ -0,0 +1,8 @@
+package fr.isen.yapagi.network
+
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class Database {
+    private val db = Firebase.database
+}
