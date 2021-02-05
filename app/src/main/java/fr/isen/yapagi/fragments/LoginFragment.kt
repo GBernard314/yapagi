5be64be02145ddeb6f8f75d502a2d5b903f4bd4d
DESOOMER Pierre <p.desoomer@gmail.com>
2021-02-05 10:49:52 +0100

    Added form validation to the login fragment

--- app/src/main/java/fr/isen/yapagi/fragments/LoginFragment.kt
+++ app/src/main/java/fr/isen/yapagi/fragments/LoginFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.afollestad.vvalidator.form
 import fr.isen.yapagi.R
+import fr.isen.yapagi.databinding.FragmentLoginBinding
+
+private lateinit var binding: FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
@@ -13,8 +18,32 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        form {
+            input(binding.etEmail) {
+                isNotEmpty()
+                length().atLeast(6)
+                matches("^[a-zA-Z0-9.!#\$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\$")
+            }
+
+            input(binding.etPassword) {
+                isNotEmpty()
+                length().atLeast(8)
+            }
+
+            submitWith(binding.btnLogin) { result ->
+                displayToast("Correctly Logged In")
+            }
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    fun displayToast(str: String) {
+        Toast.makeText(activity?.applicationContext, str, Toast.LENGTH_SHORT).show();
+    }
 }
+
