62e10fae78b26f477a0f3434f628cc9b93323ae0
BERNARD Guillaume <34131124+GBernard314@users.noreply.github.com>
2021-02-08 11:00:41 +0100

    Revert "adding floating button but ugly"
    This reverts commit 19a111e72c3b549e0adf6de1eb35a9d269818933.

--- app/src/main/java/fr/isen/yapagi/FeedActivity.kt
+++ app/src/main/java/fr/isen/yapagi/FeedActivity.kt
@@ -1,6 +1,5 @@
 package fr.isen.yapagi
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import fr.isen.yapagi.data.Comment
@@ -23,10 +22,6 @@ class FeedActivity : BaseActivity() {
         binding_feed.postList.layoutManager = LinearLayoutManager(this)
 
         makeRequest()
-
-        binding_feed.floatingActionButton.setOnClickListener {
-            startActivity(Intent(this, AddPostActivity::class.java))
-        }
 
     }
     private fun makeRequest() {
