16e53bc88b077cd3b73fdcca2c735880879de5ba
Pierre <p.desoomer@gmail.com>
2021-02-08 09:47:30 +0100

    Merge branch 'post' into addPost

--- app/src/main/java/fr/isen/yapagi/AddPostActivity.kt
+++ app/src/main/java/fr/isen/yapagi/AddPostActivity.kt
@@ -7,9 +7,11 @@ import android.os.Bundle
 import androidx.annotation.RequiresApi
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.squareup.picasso.Picasso
 import fr.isen.yapagi.data.Comment
 import fr.isen.yapagi.data.Post
 import fr.isen.yapagi.databinding.ActivityAddPostBinding
+import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
@@ -42,9 +44,14 @@ class AddPostActivity : AppCompatActivity() {
 
         if (requestCode == 777) {
             val filePath = data?.data?.path
-            println(filePath)
+            val file = File(filePath)
+            Picasso.get()
+                .load(file)
+                .placeholder(R.drawable.gear)
+                .into(binding.image)
         }
     }
+
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun createPost(): String {
